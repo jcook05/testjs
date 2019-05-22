32985ba935291bbb25f3acc38e09e8e8cd15b434
jcook05 <john.b.cook05@gmail.com>
2019-05-22 16:10:04 -0600

    making progress

---
+++ public/tests/app_spec.js
@@ -0,0 +1,11 @@
+//Tests in Jasmine
+
+describe('LearnJS', function() {
+it('can show a problem view', function() {
+
+    learnjs.showView('#problem-1');
+       expect($('.view-container .problem-view').length).toEqual(1);
+
+});
+
+});
